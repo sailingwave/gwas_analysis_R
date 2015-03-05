177d74109584af294ab9085d623afda2dc6cef99
Nan <sailingwave@gmailc.com>
2015-03-05 14:55:11 -0800

    manhattan plot finished

--- gwas_sum.R
+++ gwas_sum.R
@@ -2,18 +2,26 @@
 
 # Nan Wang
 # University of Southern California
-# ===
-# Andrew R Wood
-# Genetics of Complex Traits
-# University of Exeter Medical School
-# Email: a.r.wood@ex.ac.uk
-# Date:  25/02/2015
+# Email: sailingwave@gmail.com
 
-setwd('/Users/Nan/Desktop/')
 
-results <- 'di_all.txt'
-output  <- 'di_all.tiff'
+#=== for devlopment
+
+test = T
+
+if(test){
+    setwd('/Users/Nan/Desktop/')    
+    results <- 'di_all.txt'
+    output  <- 'di_all.tiff'
+}else{
+    args <- commandArgs(trailingOnly = TRUE)
     
+    results <- args[1]
+    output  <- args[2]
+}
+
+#==================#
+
 library(ggplot2)
 library(dplyr)
 library(data.table)
@@ -22,18 +30,21 @@ association_data<-fread(results, header=TRUE)
 setnames(association_data,names(association_data),tolower(names(association_data)))
 setkey(association_data,chr)
 
-bak = copy(association_data)
 # relabel chr X and XY = 23 if necessary
 if ("X" %in% association_data$chr) {
-    #system.time(association_data[,chr:= revalue(association_data$chr, c("X" = "23"))])    #revalue from plyr
-    association_data["X",chr:= "23"]    #save time from 6s to 0.003s
+    association_data["X",chr:= "23"]
 }
 if ("XY" %in% association_data$chr) {
-    #association_data[,chr:= revalue(association_data$chr, c("XY" = "23"))]
     association_data["XY",chr:= "23"]
 }
 
 tbl.assoc = tbl_dt(association_data)
+
+if(test){
+    bak <- copy(tbl.assoc)
+    set.seed(123)
+    tbl.assoc = sample_frac(tbl.assoc,0.0001, replace = TRUE)
+}
 
 max_pos <- tbl.assoc %>%
     group_by(chr) %>%
@@ -41,24 +52,42 @@ max_pos <- tbl.assoc %>%
     arrange(as.numeric(chr))
 setnames(max_pos,names(max_pos),c('chr','max_pos'))
 max_pos$max_pos = as.numeric(max_pos$max_pos)
+x_lab = paste0('chr',max_pos$chr)
 
 max_pos <- max_pos %>%
-    mutate(cs = cumsum(max_pos)) %>%
-    mutate(lcs = lag(cs,1,default=0))
+    mutate(cs = cumsum(max_pos),lcs = lag(cs,1,default=0),chr_mk = lcs+max_pos/2)
+#cumulative sum, lag cs, tick mark positions for each chr
 
 tbl.assoc <- tbl.assoc %>%
     left_join(max_pos) %>%
     mutate(x_pos = pos+lcs)
 
+# Manhattan plot
+n_chr = length(max_pos$chr)
+pallete = rep(c("blue4", "orange3"),times = ceiling(n_chr/2))[1:n_chr]
 
-# generate the plot
+man_plot <- ggplot(tbl.assoc, aes(x=x_pos, y=-log10(p), color=chr)) +
+    geom_vline(xintercept=max_pos$lcs[-1],linetype="dotted", size=.8,color='white') +
+    geom_hline(aes(yintercept=-log10(5e-08)),color="red") +
+    geom_point(shape=20, size = 3)+ xlab("") +
+    scale_x_continuous(breaks=max_pos$chr_mk,labels=x_lab) +
+    scale_color_manual(values=pallete,guide=FALSE) +
+    theme(panel.grid.major.x = element_blank(),panel.grid.minor = element_blank()) +
+    theme(axis.title.y=element_text(size=16)) +
+    theme(axis.text.y=element_text(size=12))
+
+
 tiff(filename=output, compression="lzw", width = 1200, height = 600, units = "px")
-print(ggplot(tbl.assoc, aes(x=x_pos, y=-log10(p), color=chr)) 
-      + geom_point(shape=20, size = 1) 
-      + xlab("") + theme(axis.text.x = element_blank(), legend.position="bottom") 
-      + scale_color_manual(values=c("blue4", "orange3")) 
-      + geom_hline(aes(yintercept=-log10(5e-08))))
+    print(man_plot)
 dev.off()
+
+
+
+# Calculate GC lambda
+
+lambda = median(qnorm(data$p/2)^2,na.rm=T)/qchisq(0.5,1) 
+print(paste("File: ", results, ": Lamda = ", lambda, sep=""))
+
 
 rm(list=ls())
 
