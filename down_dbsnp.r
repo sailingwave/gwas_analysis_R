3799362895e2ca8bc31969ff6d02eb0624f1ce3e
Nan <sailingwave@gmailc.com>
2015-03-10 17:55:45 -0700

    add the function to query chr/pos using eutils from NCBI

--- down_dbsnp.r
+++ down_dbsnp.r
@@ -7,11 +7,12 @@ library('XML')
 library('RCurl')
 
 down_dbsnp <- function(snp){
+    #from NCBI website
     url = paste0('http://www.ncbi.nlm.nih.gov/SNP/snp_ref.cgi?searchType=adhoc_search&type=rs&rs=',snp)
     dbsnp = getURL(url)
     
-    doc = htmlParse(dbsnp)
-    tabs = readHTMLTable(doc,header = F,stringsAsFactors = F)
+    doc <- htmlParse(dbsnp)
+    tabs <- readHTMLTable(doc,header = F,stringsAsFactors = F)
     
     info <<- as.data.frame(tabs[[11]])[2:14,1:2]    #general info
     
@@ -20,12 +21,38 @@ down_dbsnp <- function(snp){
     div = div[div$Pop != '',]
     div = div[-1,]
     #div[,c(2,10,11)]
+    
+    #from NCBI eutils
+    id = gsub('rs','',snp)
+    url = paste0('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=snp&report=XML&id=',id)
+    
+    file = getURL(url)
+    doc <- xmlParse(file)
+    rt = xmlRoot(doc)
+    #ns = xmlNamespaces(doc, simplify = TRUE)
+    
+    assembly = xpathSApply(rt,"//x:Assembly",xmlAttrs,namespaces = 'x')    #x is the default namespace
+    chr = xpathSApply(rt,"//x:Component",xmlGetAttr,name="chromosome",namespaces = 'x')
+    pos = xpathSApply(rt,"//x:MapLoc",xmlAttrs,namespaces = 'x')
+    
+    assem_info <<- t(rbind(assembly,chr,pos))[,c(1:3,6,11,12,15)]
 }
 
-down_dbsnp('rs8078000')
-down_dbsnp('rs13342692')
 
-a = sapply(tabs[[6]],function(x) grep('Chr Pos',unlist(x)))
-which(sapply(a,length)!=0)
+
+#usage
+
+down_dbsnp('rs8078000')    #create info and div in the global env
+info
+div
+
+
+#dev
+down_dbsnp('rs8078000')
+
+#a = sapply(tabs,function(x) grep('7044260',unlist(x)))
+#which(sapply(a,length)!=0)
+
+
 
 
