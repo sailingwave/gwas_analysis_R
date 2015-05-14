d731feb268760b9899f65fb76c7ae784c350f83d
Nan <sailingwave@gmail.com>
2015-05-13 23:35:49 -0700

    complete qq plot part

--- gwas_sum.R
+++ gwas_sum.R
@@ -19,8 +19,8 @@ test = F
 if(test){
     setwd('/Users/Nan/Desktop/')    
     results <- 'test.txt'
-    output_mh  <- 'test.tiff'
-    output_qq  <- 'test.tiff'
+    output_mh  <- 'test.png'
+    output_qq  <- 'test.png'
 }else{
     args <- commandArgs(trailingOnly = TRUE)
     
@@ -36,11 +36,11 @@ library(ggplot2)
 library(data.table)
 library(dplyr)
 
-PlotEnv <- new.env(parent = emptyenv())
+#PlotEnv <- new.env(parent = emptyenv())
 
 
 #=== Data preparation
-data_prep <- function(results=results){
+data_prep <- function(results_arg){
     association_data<<-fread(results, header=TRUE)
     setnames(association_data,names(association_data),tolower(names(association_data)))
     setkey(association_data,chr)
@@ -55,6 +55,14 @@ data_prep <- function(results=results){
     
     tbl.assoc <<- tbl_dt(association_data)
     
+    if(class(tbl.assoc$chr)=='character'){
+        tbl.assoc[,chr:=as.numeric(chr)]
+    }
+    
+    if(class(tbl.assoc$pos)=='character'){
+        tbl.assoc[,pos:=as.numeric(gsub('[^0-9]','',pos))] 
+    }
+    
     if(test){    #for developing
         bak <- copy(tbl.assoc)
         set.seed(123)
@@ -63,14 +71,21 @@ data_prep <- function(results=results){
 }
 
 
-mhplot <- function(results=results,output_mh=output_mh,
+mhplot <- function(results_arg=results,output_mh_arg=output_mh,
                    fast_plot = T,    #whether plot a small fraction of SNPs below a p value cutoff
-                   p_cut = 1e-2,snp_frac = 0.05,
+                   p_cut = 1e-2,snp_frac = 0.1,
                    plot_to_console=F){
 
-    if(!exists('association_data')){
-        data_prep(results=results)
+    if(is.na(output_mh)){
+        cat("No MH plot specified.\n")
+        return(NULL)
     }
+    
+    if(!exists('association_data')){
+        data_prep(results_arg=results_arg)
+    }
+    
+    cat("Generating Manhattan plot ... ")
     
     #===========#
     chr_sep = 3e7    #separation among chromosomes for mh plot, in base pairs; can be set to 0 if no separation is wanted
@@ -107,106 +122,141 @@ mhplot <- function(results=results,output_mh=output_mh,
     pallete[pallete == '1'] <- "blue4"
     pallete[pallete == '0'] <- "orange3"
     
-    if(!is.na(output_mh)){
-        cat("Generating Manhattan plot ... ")
+    
+    if(fast_plot){    #for fast plot
+        set.seed(888)
+        sample_snp <- tbl.assoc %>%
+            filter(p >= p_cut) %>%
+            sample_frac(snp_frac, replace = F)
         
-        if(fast_plot){    #for fast plot
-            set.seed(888)
-            sampl_snp <- tbl.assoc %>%
-                filter(p >= p_cut) %>%
-                sample_frac(snp_frac, replace = F)
-            
-            mh_data <- tbl.assoc %>%
-                filter(p < p_cut) %>%
-                rbind(sampl_snp)
-        }else{
-            mh_data <- tbl.assoc
-        }
-        
-        runtime = system.time(
-        {
-            mh_plot <- ggplot(mh_data, aes(x=x_pos, y=-log10(p), color=chr)) +
-                geom_vline(xintercept=max_pos_dat$chr_sep_line[-1],linetype="dotted", size=.8,color='white') +
-                geom_hline(aes(yintercept=-log10(5e-08)),color="red") +
-                geom_point(aes(color=factor(chr)),shape=20, size = 3)+ xlab("") +
-                scale_x_continuous(breaks=max_pos_dat$chr_mk,labels=x_lab) +
-                scale_y_continuous(breaks=seq(0, 12, 2)) +
-                scale_color_manual(values=pallete,guide=FALSE) +
-                theme(panel.grid.major.x = element_blank(),panel.grid.minor = element_blank()) +
-                theme(axis.title.y=element_text(size=20)) +
-                theme(axis.text.y=element_text(size=16)) +
-                theme(axis.text.x=element_text(size=16))
-            
-            if(!plot_to_console){
-                tiff(filename=output_mh, compression="lzw", width = 1200, height = 600, units = "px")
-            }
-            
-            print(mh_plot)
-            
-            if(!plot_to_console){
-                dev.off()
-            }
-        })[3]
-        
-        cat(paste(" in",runtime," s\n"))
+        mh_data <- tbl.assoc %>%
+            filter(p < p_cut) %>%
+            rbind(sample_snp)
     }else{
-        cat("No plot specified.\n")
+        mh_data <- tbl.assoc
     }
+    
+    runtime = system.time(
+    {
+    mh_plot <- ggplot(mh_data, aes(x=x_pos, y=-log10(p), color=chr)) +
+        geom_vline(xintercept=max_pos_dat$chr_sep_line[-1],linetype="dotted", size=.8,color='white') +
+        geom_hline(aes(yintercept=-log10(5e-08)),color="red") +
+        geom_point(aes(color=factor(chr)),shape=20, size = 3)+ xlab("") +
+        scale_x_continuous(breaks=max_pos_dat$chr_mk,labels=x_lab) +
+        scale_y_continuous(breaks=seq(0, 12, 2)) +
+        scale_color_manual(values=pallete,guide=FALSE) +
+        theme(panel.grid.major.x = element_blank(),panel.grid.minor = element_blank()) +
+        theme(axis.title.y=element_text(size=20)) +
+        theme(axis.text.y=element_text(size=16)) +
+        theme(axis.text.x=element_text(size=16))
+    
+        if(!plot_to_console){
+            png(filename=output_mh, width = 1200, height = 600, units = "px")
+        }
+    
+        print(mh_plot)
+    
+        if(!plot_to_console){
+            dev.off()
+        }
+    })[3]
+
+    cat(paste(" in",runtime," s\n"))
 }
 
 if(length(args)>0){
     mhplot()
 }
 
+
 #=== QQ plot
-# 
-# if(!is.na(output_qq)){
-#     cat("Generating QQ plot ... ")
-#     
-# 
-#     alpha = 0.05    #alpha level for confidence interval
-#     
-#     exp = -log10(1:n_snp/n_snp)
-#     obs = -log10(sort(tbl.assoc$p))
-#         
-#     #expected (null) p values ~ unif(0,1), of which the order stat ~ beta(i,N-i+1)
-#     ci_up=-log10(qbeta(1-alpha/2, 1:n_snp, (n_snp-1:n_snp+1)))
-#     ci_low=-log10(qbeta(alpha/2, 1:n_snp, (n_snp-1:n_snp+1)))
-#         
-#     lab_exp = expression(paste("Expected -log"[10], plain(P)))
-#     lab_obs = expression(paste("Observed -log"[10], plain(P)))
-# 
-#     
-#     
-#     
-#     runtime = system.time(
-#     {
-#         qq_plot <- ggplot(tbl.assoc) +
-#             theme_bw() +
-#             geom_ribbon(aes(x=exp, ymin=ci_up, ymax=ci_low), fill="grey", alpha=.5) +
-#             geom_point(aes(exp, obs), shape=1, size=3) +
-#             geom_abline(intercept=0, slope=1, alpha=0.8 ,color='red') +     
-#             xlab(lab_exp) + ylab(lab_obs) +
-#             theme(axis.title=element_text(size=16)) +
-#             theme(axis.text=element_text(size=12))
-#         
-#         
-#         tiff(output_qq, compression="lzw")
-#             print(qq_plot)
-#         dev.off()
-#     })[3]
-#     
-#     cat(paste(" in",runtime," s\n"))
-# }
-# 
-# 
-# #=== Calculate GC lambda
-# 
-# lambda = median(qchisq(tbl.assoc$p,df = 1,lower.tail = F),na.rm = T)/qchisq(0.5,df = 1) 
-# print(paste0("File: ", results, " => GC Lamda = ", lambda))
-# 
-# 
-# #=== Finish
-# 
-# rm(list=ls())
+
+
+myQQplot <- function(results_arg=results,output_qq_arg=output_qq,
+                   fast_plot = T,    #whether plot a small fraction of SNPs below a p value cutoff
+                   p_cut = 1e-3,snp_frac = 0.05,
+                   plot_to_console=F){
+
+    if(is.na(output_mh)){
+        cat("No MH plot specified.\n")
+        return(NULL)
+    }
+    
+    if(!exists('association_data')){
+        data_prep(results_arg=results_arg)
+    }
+        
+    cat("Generating QQ plot ... ")      
+    
+    n_snp = nrow(tbl.assoc)
+    
+    alpha = 0.05    #alpha level for confidence interval
+        
+    exp_total = -log10(1:n_snp/n_snp)
+    
+    #expected (null) p values ~ unif(0,1), of which the order stat ~ beta(i,N-i+1)
+    ci_up = -log10(qbeta(1-alpha/2, 1:n_snp, (n_snp-1:n_snp+1)))
+    ci_low = -log10(qbeta(alpha/2, 1:n_snp, (n_snp-1:n_snp+1)))
+    
+    lab_exp = expression(paste("Expected -log"[10], plain(P)))
+    lab_obs = expression(paste("Observed -log"[10], plain(P)))
+    
+    qq_data <- tbl.assoc %>%
+        mutate(obs = -log10(p)) %>%
+        select(obs) %>%
+        arrange(desc(obs)) %>%
+        cbind(exp_total,ci_up,ci_low)
+    
+    setnames(qq_data,names(qq_data),c('obs','exp','ci_up','ci_low'))
+    
+    if(fast_plot){    #for fast plot
+        set.seed(888)
+               
+        sample_snp <- qq_data %>%
+            filter(obs <= -log10(p_cut)) %>%
+            sample_frac(snp_frac)
+        
+        qq_data <- qq_data %>%
+            filter(obs > -log10(p_cut)) %>%
+            rbind(sample_snp)
+    } 
+    
+    runtime = system.time(
+    {
+    qq_plot <- ggplot(qq_data) +
+        theme_bw() +
+        geom_ribbon(aes(x=exp, ymin=ci_up, ymax=ci_low), fill="grey", alpha=.5) +
+        geom_point(aes(exp, obs), shape=1, size=3) +
+        geom_abline(intercept=0, slope=1, alpha=0.8 ,color='red') +     
+        xlab(lab_exp) + ylab(lab_obs) +
+        theme(axis.title=element_text(size=16)) +
+        theme(axis.text=element_text(size=12))
+    
+        if(!plot_to_console){
+            png(output_qq, width = 800, height = 800, units = "px")
+        }
+    
+        print(qq_plot)
+    
+        if(!plot_to_console){
+            dev.off()
+        }
+    })[3]
+
+    cat(paste(" in",runtime," s\n"))
+}
+
+if(length(args)>0){
+    myQQplot(plot_to_console=T)
+}
+
+#=== Calculate GC lambda
+
+lambda = median(qchisq(tbl.assoc$p,df = 1,lower.tail = F),na.rm = T)/qchisq(0.5,df = 1) 
+print(paste0("File: ", results, " => GC Lamda = ", lambda))
+
+
+#=== Finish
+
+rm(list=ls())
 
